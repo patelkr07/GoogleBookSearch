2c2e8d5485c75fc7d48f143fc690191126db27ee
Kunal <patelkr07@gmail.com>
2019-07-17 19:57:50 -0500

    added base template files

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,34 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import logo from './logo.svg';
+import Books from "./pages/Books";
+import Nav from "./components/Nav"
 import './App.css';
+
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Books} />
+          <Route exact path="/books" component={Books} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
 
 // function App() {
 //   return (
@@ -23,21 +50,3 @@ import './App.css';
 //     </div>
 //   );
 // }
-
-function App() {
-  return (
-    <Router>
-      <div>
-        <Nav />
-        <Switch>
-          <Route exact path="/" component={Books} />
-          <Route exact path="/books" component={Books} />
-          <Route exact path="/books/:id" component={Detail} />
-          <Route component={NoMatch} />
-        </Switch>
-      </div>
-    </Router>
-  );
-}
-
-export default App;
