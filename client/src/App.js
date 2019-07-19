25165445ffe29ec2ef76234986fe6302026a0e39
Kunal <patelkr07@gmail.com>
2019-07-19 16:42:33 -0500

    fixing App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,169 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Books from "./pages/Books";
-import Nav from "./components/Nav"
-import './App.css';
+import React, { Component } from "react";
+import Jumbotron from "./components/Jumbotron";
+import Nav from "./components/Nav";
+import Input from "./components/Input"
+import Button from "./components/Button"
+import API from "./utils/API";
+import { BookList, BookListItem } from "./components/BookList";
+import { Container, Row, Col } from "./components/Grid";
 
+class App extends Component {
+  state = {
+    books: [],
+    bookSearch: ""
+  };
 
-function App() {
-  return (
-    <Router>
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    API.getBooks(this.state.bookSearch)
+    .then(res => this.setState({ books: res.data }))
+    .catch(err => console.log(err));
+
+  };
+
+  render() {
+    return (
       <div>
         <Nav />
-        <Switch>
-          <Route exact path="/" component={Books} />
-          <Route exact path="/books" component={Books} />
-        </Switch>
+        <Jumbotron />
+        <Container>
+          <Row>
+            <Col size="md-12">
+              <form>
+                <Container>
+                  <Row>
+                    <Col size="xs-9 sm-10">
+                      <Input
+                        name="bookSearch"
+                        value={this.state.bookSearch}
+                        onChange={this.handleInputChange}
+                        placeholder="Search for a Book"
+                      />
+                    </Col>
+                    <Col size="xs-3 sm-2">
+                      <Button
+                        onClick={this.handleFormSubmit}
+                        type="success"
+                        className="input-lg"
+                      >
+                        Search
+                      </Button>
+                    </Col>
+                  </Row>
+                </Container>
+              </form>
+            </Col>
+          </Row>
+            <Col size="xs-12">
+              {!this.state.books.length ? (
+                <h1 className="text-center">No Books to Display</h1>
+              ) : (
+                <BookList>
+                  {this.state.books.map(book => {
+                    return (
+                      <BookListItem
+                        key={book.title}
+                        title={book.title}
+                        authors={book.authors}
+                        description={book.description}
+                        image={book.image}
+                        link={book.link}
+                        thumbnail={book.thumbnail}
+                      />
+                    );
+                  })}
+                </BookList>
+              )}
+            </Col>
+        </Container>
       </div>
-    </Router>
-  );
+    );
+  }
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import Books from "./pages/Books";
+// import Nav from "./components/Nav"
+// import './App.css';
+
+
+// function App() {
+//   return (
+//     <Router>
+//       <div>
+//         <Nav />
+//         <Switch>
+//           <Route exact path="/" component={Books} />
+//           <Route exact path="/books" component={Books} />
+//         </Switch>
+//       </div>
+//     </Router>
+//   );
+// }
+
+// export default App;
 
 
 
