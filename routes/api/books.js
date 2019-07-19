eec495dde0d0fc7c42479c9a07d5b83df937017f
Kunal <patelkr07@gmail.com>
2019-07-19 15:48:29 -0500

    deleted components, re-building

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,3 +1,15 @@
 const router = require("express").Router();
 const booksController = require("../../controllers/booksController")
 
+router.route("/")
+    .get(booksController.findAll)
+    .post(booksController.create)
+
+
+router
+    .route("/:id")
+    .delete(booksController.remove)
+
+module.exports = router;
+
+
